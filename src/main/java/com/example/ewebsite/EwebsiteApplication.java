d258ef2e21725e607c4704813a5dbadcb918e372
pasindu <www.pasindudenuka5770@gmail.com>
2024-03-12 21:43:40 +0530

    create project

---
+++ src/main/java/com/example/ewebsite/EwebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.ewebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ewebsite/EwebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ewebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
